319426961bc4326e4d70e7c0d436c9f0de3851e0
Yodra <YodraB@users.noreply.github.com>
2020-05-07 09:55:02 -0400

    renames

--- processing/sketch.js
+++ processing/sketch.js
@@ -1,8 +1,16 @@
 function setup() {
   // put setup code here
+  size = 800;
+	createCanvas(size, size);
+	background(0, 153, 255);
+	mask = loadImage("images/Khundii_BasePaint_White.png");
+	//mane = loadImage("KhundiiMane.png");
+	//lines = loadImage("KhundiiLines.png")
 }
 
 function draw() {
   // put drawing code here
-  ellipse(50, 50, 80, 80);
+  //ellipse(50, 50, 80, 80);
+  image(mask, 0, 0, size, size);
+  tint(204, 229, 255);
 }
